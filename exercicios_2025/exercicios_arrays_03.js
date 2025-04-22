8ae41814f6fff5aa34004b0253bf1312127cafbd
André Santos <andremissional@gmail.com>
2025-04-22 09:35:47 -0300

    Update exercicios_arrays_03.js

--- exercicios_2025/exercicios_arrays_03.js
+++ exercicios_2025/exercicios_arrays_03.js
@@ -5,6 +5,6 @@ const jogadores = ['Arraxcaeta','Gerson','Pedro','Wesley','BH','Léo Ortis'];
 
 console.log(jogadores)
 
-for(i = 0; i < jogadores.length; i++){
-  console.log('Os melhores jogadores do flamengo são: ' + jogadores[i]);
+for (let i = 0; i < jogadores.length; i++){
+  console.log(`Os melhores jogadores do Flamengo são: ` ${jogadores[i]});
 }
