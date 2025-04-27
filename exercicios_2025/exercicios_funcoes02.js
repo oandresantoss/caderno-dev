d7c0900dbc727159df31feface21e53e6fe46729
André Santos <andremissional@gmail.com>
2025-04-27 19:42:38 -0300

    Create exercicios_funcoes02.js

---
+++ exercicios_2025/exercicios_funcoes02.js
@@ -0,0 +1,26 @@
+
+
+//Vou fzer uma função que recebe uma lista de cadartro simples e altera ela
+
+
+
+let cadastro = [
+    {
+        nome: 'André',
+        idade: 37,
+    }
+]
+
+console.log(cadastro); //cadastro antes da alteração
+
+const CadaAtualizado = (novosDados) => {
+    Object.assign(cadastro[0], novosDados);
+};
+
+cadAtualizado({
+    nome: 'Felipe',
+    idade: 37,
+});
+
+
+console.log(cadastro);
