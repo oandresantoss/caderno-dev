803582f85d26069cbf757c212fb73680619fed01
André Santos <andremissional@gmail.com>
2025-04-27 19:41:58 -0300

    Revert "Update exercicios_funcoes01.js"
    This reverts commit 2462844497b69f2d8cea033154f02b5134916adf.

--- exercicios_2025/exercicios_funcoes01.js
+++ exercicios_2025/exercicios_funcoes01.js
@@ -12,11 +12,11 @@ let cadastro = [
 ]
 
 console.log(cadastro); //cadastro antes da alteração
-// cria uma cópia origi8nal do objeto antes de modificá-lo
-const cadAtualizado = (novosDados) => {
+
+const CadaAtualizado = (novosDados) => {
     Object.assign(cadastro[0], novosDados);
 };
-// dados novos 
+
 cadAtualizado({
     nome: 'Felipe',
     idade: 37,
