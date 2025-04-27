9e9bf6f33fd151ded57959fd2141d0b20de88a3f
André Santos <andremissional@gmail.com>
2025-04-27 20:59:50 -0300

    Update exercicios_funcoes02.js

--- exercicios_2025/exercicios_funcoes02.js
+++ exercicios_2025/exercicios_funcoes02.js
@@ -1,1 +1,19 @@
 
+
+
+
+function nome (){
+    console.log('Estou dentro da função')
+
+}
+
+nome()
+
+
+
+function soma (n1,n2){
+    const somaNumeros = n1 + n2
+    console.log(`A soma dos números é: ${soma}`)
+}
+
+soma(324,545)
