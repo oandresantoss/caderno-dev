65f69a1715db08fbb21d8b018e44b4386597069c
André Santos <andremissional@gmail.com>
2025-04-27 14:59:36 -0300

    Create funcao002.js

---
+++ desafios_2025/funcao002.js
@@ -0,0 +1,16 @@
+
+
+let cadastro = [{
+    nome: ('André'),
+    idade: (37),
+    sexo: ('M'),
+    casado: ('SIM'),
+    mae: ('Maria Joselia'),
+
+}]
+
+console.log(cadastro)
+
+const cadAtualizado = () => {
+    () => ('')
+}
