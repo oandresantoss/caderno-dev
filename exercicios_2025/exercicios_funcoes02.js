faa9ab4d845b0adbe555e8d1116d083052043d7d
André Santos <andremissional@gmail.com>
2025-04-27 21:06:30 -0300

    Update exercicios_funcoes02.js

--- exercicios_2025/exercicios_funcoes02.js
+++ exercicios_2025/exercicios_funcoes02.js
@@ -1,21 +1,28 @@
 
 
-
-/* Função VOID vazia
+/*Função VOID vazia
 function nome (){
     console.log('Estou dentro da função')
 
 }
 
 nome()
+
+
+let valor = 20
+
+function incrementa(){
+    valor = valor + 35
+}
+
+incrementa()
+console.log(valor)
 */
 
 
 function soma (n1,n2){
     const somaNumeros = n1 + n2
-    console.log(`A soma dos números é: ${soma}`)
+    console.log(`A soma dos números é: ${somaNumeros}`)
 }
 
 soma(324,545)
-
-
